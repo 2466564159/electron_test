b2872acf07e2a6f84ababb5679cd61b4aefdb969
The proletarian warrior <2466564159@qq.com>
2023-11-22 22:18:39 +0800

    first_electron app

---
+++ preload.js
@@ -0,0 +1,9 @@
+// const process = require('node:process')
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron,
+    ping: () => ipcRenderer.invoke('ping')
+})
